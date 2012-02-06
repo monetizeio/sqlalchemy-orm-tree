5b1ded9751d9350c3f6823181f61f27413e1b626
Mark Friedenbach <mark@roku-sigma.com>
2012-02-05 18:02:29 -0800

    Moved version data into setup.py so as to avoid a requirement on sqlalchemy during install.

--- setup.py
+++ setup.py
@@ -37,7 +37,20 @@ import os
 
 from distutils.core import setup
 
-from sqlalchemy_tree import get_version
+VERSION = (0,0,1, 'final', 0)
+
+def get_version():
+  version = '%s.%s' % (VERSION[0], VERSION[1])
+  if VERSION[2]:
+    version = '%s.%s' % (version, VERSION[2])
+  if VERSION[3:] == ('alpha', 0):
+    version = '%s pre-alpha' % version
+  else:
+    if VERSION[3] != 'final':
+      version = '%s %s' % (version, VERSION[3])
+      if VERSION[4] != 0:
+        version = '%s%s' % (version, VERSION[4])
+  return version
 
 # Compile the list of packages available, because distutils doesn't have an
 # easy way to do this.
--- sqlalchemy_tree/__init__.py
+++ sqlalchemy_tree/__init__.py
@@ -99,21 +99,6 @@ from .options    import *
 from .orm        import *
 from .types      import *
 
-VERSION = (0,0,1, 'final', 0)
-
-def get_version():
-  version = '%s.%s' % (VERSION[0], VERSION[1])
-  if VERSION[2]:
-    version = '%s.%s' % (version, VERSION[2])
-  if VERSION[3:] == ('alpha', 0):
-    version = '%s pre-alpha' % version
-  else:
-    if VERSION[3] != 'final':
-      version = '%s %s' % (version, VERSION[3])
-      if VERSION[4] != 0:
-        version = '%s%s' % (version, VERSION[4])
-  return version
-
 # ===----------------------------------------------------------------------===
 # End of File
 # ===----------------------------------------------------------------------===
